405c793c7595873cf1b24d3fca5d0b88d65bda2f
Nima Shoghi <nimashoghi@gmail.com>
2024-09-05 20:39:43 -0400

    Remove cached path support -> moved to nshconfig-extra

--- src/nshconfig/__init__.py
+++ src/nshconfig/__init__.py
@@ -1,6 +1,5 @@
 from pydantic import *  # type: ignore  # noqa: F403
 
-from . import types as types
 from ._config import Config as Config
 from ._missing import MISSING as MISSING
 from ._missing import AllowMissing as AllowMissing
